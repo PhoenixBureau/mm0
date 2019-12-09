375084fc47092e5483c2e9966116e8e41073d6bd
Simon Forman <sforman@hushmail.com>
2019-12-08 16:45:39 -0800

    sort-stmt

--- mm0-prolog/mm0.pl
+++ mm0-prolog/mm0.pl
@@ -1,11 +1,14 @@
-% :- use_module(library(dcg/basics)).
-% high_order
 
 
-mm0_file([LEX|Rest]) --> lexeme(LEX), mm0_file(Rest).
-mm0_file(     Rest ) --> whitespace,  mm0_file(Rest).
-mm0_file([])         --> [], !.
+mm0_file([Statement|Rest]) --> statement(Statement), mm0_file(Rest).
+mm0_file([]) --> [], !.
 
+
+% Lexxer.
+
+lex([LEX|Rest]) --> lexeme(LEX), lex(Rest).
+lex(     Rest ) --> whitespace,  lex(Rest).
+lex([])         --> [], !.
 
 % lexeme ::= symbol | identifier | number | math-string
 
@@ -64,6 +67,7 @@ line_comment --> "--", non_newlines, "\n".
 
 non_newlines --> [Ch], { nonvar(Ch), Ch =\= "\n" }, non_newlines.
 non_newlines --> [], !.
+% Compare with mstr//3.
 
 
 % Character recognizers
@@ -76,6 +80,28 @@ alpha(Ch) --> [Ch], { nonvar(Ch), (between(0'a, 0'z, Ch) | between(0'A, 0'Z, Ch)
 underscore("_") --> "_".
 
 
+
+% Parser (or wahtever)
+
+statement(Statement) --> sort_stmt(Statement)
+    |  term_stmt(Statement)
+    |  assert_stmt(Statement)
+    |  def_stmt(Statement)
+    |  notation_stmt(Statement)
+    |  inout_stmt(Statement).
+
+
+% sort-stmt ::= ('pure')? ('strict')? ('provable')? ('free')? 'sort' identifier ';'
+
+sort_stmt(sort(Name, Opts)) --> opts(Opts), [ident(sort), ident(Name), symbol(;)].
+
+opts([    pure|Rest]) --> [ident(pure)], opts(Rest).
+opts([  strict|Rest]) --> [ident(strict)], opts(Rest).
+opts([provable|Rest]) --> [ident(provable)], opts(Rest).
+opts([    free|Rest]) --> [ident(free)], opts(Rest).
+opts([]) --> [], !.
+
+
 % Try it out...
 
 mm0_filename("../examples/hello.mm0").
@@ -83,5 +109,5 @@ mm0_filename("../examples/hello.mm0").
 do(MM0, R) :-
     mm0_filename(FN),
     read_file_to_codes(FN, Codes, []),
-    phrase(mm0_file(MM0), Codes, Rest),
+    phrase(lex(MM0), Codes, Rest),
     atom_codes(R, Rest).
